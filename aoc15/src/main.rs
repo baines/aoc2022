37d9e8d9f26db2b83878dcff782dd0e48b4322b9
Alex Baines <alex@abaines.me.uk>
2022-12-16 21:24:33 +0000

    day 15 part 2
    not sure if this is a good approach but it worked pretty fast for my input

--- aoc15/src/main.rs
+++ aoc15/src/main.rs
@@ -10,10 +10,12 @@ struct Pos {
 }
 
 impl Pos {
-    fn manhattan_distance(&self, pos: &Pos) -> u64 {
-        self.x.abs_diff(pos.x) + self.y.abs_diff(pos.y)
+    fn manhattan_distance(&self, pos: &Pos) -> i64 {
+        (self.x.abs_diff(pos.x) + self.y.abs_diff(pos.y)) as i64
     }
 }
+
+static MAX_SIZE: i64 = 4000000;
 
 struct World {
     reports: Vec<SensorReport>,
@@ -26,11 +28,11 @@ impl World {
         let (min, max) = reports.iter().fold((Pos{x: i64::MAX, y: i64::MAX}, Pos{x: i64::MIN, y: i64::MIN}), |acc, r| {
             let mut result = acc;
 
-            result.0.x = min(min(result.0.x, r.sensor_pos.x - r.distance as i64), r.beacon_pos.x);
-            result.0.y = min(min(result.0.y, r.sensor_pos.y - r.distance as i64), r.beacon_pos.y);
+            result.0.x = min(min(result.0.x, r.sensor_pos.x - r.distance), r.beacon_pos.x);
+            result.0.y = min(min(result.0.y, r.sensor_pos.y - r.distance), r.beacon_pos.y);
 
-            result.1.x = max(max(result.1.x, r.sensor_pos.x + r.distance as i64), r.beacon_pos.x);
-            result.1.y = max(max(result.1.y, r.sensor_pos.y + r.distance as i64), r.beacon_pos.y);
+            result.1.x = max(max(result.1.x, r.sensor_pos.x + r.distance), r.beacon_pos.x);
+            result.1.y = max(max(result.1.y, r.sensor_pos.y + r.distance), r.beacon_pos.y);
 
             result
         });
@@ -40,33 +42,137 @@ impl World {
         }
     }
 
-    fn check_row(&self, y: i64) -> usize {
-        let mut count = 0;
+    fn find_the_fish(&self) -> Option<Pos> {
+        let count = self.reports.len();
 
-        for x in self.min.x..(self.max.x+1) {
-            let mut found_beacon = false;
-            let pos = Pos { x, y };
+        for (i, r) in self.reports.iter().enumerate() {
+            let mut iter = ReportPerimeterIter::new(r);
 
-            for r in &self.reports {
-                if r.beacon_pos == pos {
-                    found_beacon = true;
-                    break;
+            println!("checking report perimeter {} of {}...", i, count);
+
+            while let Some(p) = iter.next() {
+                if p.x < 0 || p.y > 4000000 {
+                    continue;
                 }
-            }
 
-            if found_beacon {
-                continue;
-            }
+                let mut found = true;
 
-            for r in &self.reports {
-                if r.in_range(&pos) {
-                    count += 1;
-                    break;
+                for r2 in &self.reports {
+                    if r2.in_range(&p) {
+                        found = false;
+                        break;
+                    }
+                }
+
+                if found {
+                    return Some(p);
                 }
             }
         }
 
-        count
+        None
+    }
+}
+
+struct ReportPerimeterIter<'a> {
+    report: &'a SensorReport,
+    pos: Option<Pos>,
+}
+
+impl<'a> ReportPerimeterIter<'a> {
+    fn new(report: &'a SensorReport) -> Self {
+        ReportPerimeterIter {
+            report,
+            pos: None
+        }
+    }
+}
+
+fn constrain_pos(p: &mut Pos) {
+    if p.x < 0 {
+        p.y += p.x;
+        p.x = 0;
+    }
+
+    if p.y < 0 {
+        p.x -= p.y;
+        p.y = 0;
+    }
+
+    if p.x > MAX_SIZE {
+        p.y += MAX_SIZE - p.x;
+        p.x = MAX_SIZE;
+    }
+
+    if p.y > MAX_SIZE {
+        p.x -= MAX_SIZE - p.y;
+        p.y = MAX_SIZE;
+    }
+}
+
+impl<'a> Iterator for ReportPerimeterIter<'a> {
+    type Item = Pos;
+
+    fn next(&mut self) -> Option<Self::Item> {
+        let org = self.report.sensor_pos;
+        let r = self.report.distance + 1;
+
+        if org.x - r > MAX_SIZE || org.x + r < 0 || org.y - r > MAX_SIZE || org.y + r < 0 {
+            return None;
+        }
+
+        let mut initialpos = Pos { x: org.x - r, y: org.y };
+        constrain_pos(&mut initialpos);
+
+        let mut nextpos = {
+            if let Some(p) = self.pos {
+
+                // clockwise
+
+                if p.x < org.x {
+                    if p.y <= org.y {
+                        // top left
+                        Pos { x: p.x + 1, y: p.y - 1 }
+                    } else {
+                        // bottom left
+                        Pos { x: p.x - 1, y: p.y - 1 }
+                    }
+                } else if p.x == org.x {
+                    if p.y < org.y {
+                        // top
+                        Pos { x: p.x + 1, y: p.y + 1 }
+                    } else {
+                        // bottom
+                        Pos { x: p.x - 1, y: p.y - 1 }
+                    }
+                } else {
+                    if p.y < org.y {
+                        // top right
+                        Pos { x: p.x + 1, y: p.y + 1 }
+                    } else {
+                        // bottom right
+                        Pos { x: p.x - 1, y: p.y + 1 }
+                    }
+                }
+            } else {
+                initialpos
+            }
+        };
+
+        constrain_pos(&mut nextpos);
+
+        if let Some(_) = self.pos {
+            if nextpos == initialpos {
+                println!("yaay");
+                return None;
+            }
+        }
+
+        //println!("pos = {},{}", nextpos.x, nextpos.y);
+
+        self.pos = Some(nextpos);
+
+        return Some(nextpos);
     }
 }
 
@@ -74,7 +180,7 @@ impl World {
 struct SensorReport {
     sensor_pos: Pos,
     beacon_pos: Pos,
-    distance: u64,
+    distance: i64,
 }
 
 impl SensorReport {
@@ -116,6 +222,8 @@ fn main() {
     let reports: Vec<SensorReport> = input.lines().map(|l| l.parse().unwrap()).collect();
     let world = World::new(reports);
 
-    let y = 2000000;
-    println!("count = {}", world.check_row(y));
+    if let Some(p) = world.find_the_fish() {
+        println!("hidden beacon pos = {},{}. freq={}", p.x, p.y, p.x * MAX_SIZE + p.y);
+    }
+
 }
